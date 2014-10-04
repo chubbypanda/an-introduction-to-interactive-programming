fc94de3eb4fb6e01432ff3a261e7e3348249287e
chubbypanda <kmoravec@gmail.com>
2014-10-04 18:11:49 +0800

    Mini-project 0 "We want... a shrubbery!

---
+++ mini_project0.py
@@ -0,0 +1,5 @@
+# Mini-project 0 for An Introduction to Interactive Programming in Python class, by k., 09/21/2014
+# "We want... a shrubbery! 
+# https://class.coursera.org/interactivepython-005/human_grading/view/courses/972530/assessments/27/submissions
+
+print('We want... a shrubbery!')
